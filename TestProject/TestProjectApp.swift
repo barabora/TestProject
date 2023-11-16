1f852af04dbf9b82641b3f037c78391244d7a51a
CHANG YOUNG JIN <barabora@me.com>
2023-11-16 12:25:10 +0900

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by padosori on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
